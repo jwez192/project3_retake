06adb02b1ff64296b35b5c6988fdd2d51fa29606
jaywest97 <jewesterwelle@student.fullsail.edu>
2021-07-28 19:40:15 -0700

    testing character images

--- js/character.js
+++ js/character.js
@@ -19,145 +19,145 @@
                     break;
                 case "charTwo":
                     console.log("2");
-                    document.getElementById('charCol').style.backgroundImage = "url(../images/characters/icemechanic/Icemechanic.jpg)";
+                    document.getElementById('charCol').style.backgroundImage = "url('../images/characters/icemechanic/Icemechanic.jpg')";
                     injectItemLeft = '';
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.head}" class="itemImg"></div>
-                    <p>${characters[1].equipment.head}</p>
+                    <p class = "descriptionText">${characters[1].equipment.head}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.neck}" class="itemImg"></div>
-                    <p>${characters[1].equipment.neck}</p>
+                    <p class = "descriptionText">${characters[1].equipment.neck}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.shoulders}" class="itemImg"></div>
-                    <p>${characters[1].equipment.shoulders}</p>
+                    <p class = "descriptionText">${characters[1].equipment.shoulders}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.cloak}" class="itemImg"></div>
-                    <p>${characters[1].equipment.cloak}</p>
+                    <p class = "descriptionText">${characters[1].equipment.cloak}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.chest}" class="itemImg"></div>
-                    <p>${characters[1].equipment.chest}</p>
+                    <p class = "descriptionText">${characters[1].equipment.chest}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.shirt}" class="itemImg"></div>
-                    <p>${characters[1].equipment.shirt}</p>
+                    <p class = "descriptionText">${characters[1].equipment.shirt}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.tabard}" class="itemImg"></div>
-                    <p>${characters[1].equipment.tabard}</p>
+                    <p class = "descriptionText">${characters[1].equipment.tabard}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[1].equipment.wrists}" class="itemImg"></div>
-                    <p>${characters[1].equipment.wrists}</p>
+                    <p class = "descriptionText">${characters[1].equipment.wrists}</p>
                     </article>`;
                     injectLocationLeft.innerHTML = injectItemLeft;
 
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.gloves}" class="itemImg"></div>
-                    <p>${characters[1].equipment.gloves}</p>
+                    <p class = "descriptionText">${characters[1].equipment.gloves}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.belt}" class="itemImg"></div>
-                    <p>${characters[1].equipment.belt}</p>
+                    <p class = "descriptionText">${characters[1].equipment.belt}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.pants}" class="itemImg"></div>
-                    <p>${characters[1].equipment.pants}</p>
+                    <p class = "descriptionText">${characters[1].equipment.pants}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.boots}" class="itemImg"></div>
-                    <p>${characters[1].equipment.boots}</p>
+                    <p class = "descriptionText">${characters[1].equipment.boots}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.ringOne}" class="itemImg"></div>
-                    <p>${characters[1].equipment.ringOne}</p>
+                    <p class = "descriptionText">${characters[1].equipment.ringOne}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.ringTwo}" class="itemImg"></div>
-                    <p>${characters[1].equipment.ringTwo}</p>
+                    <p class = "descriptionText">${characters[1].equipment.ringTwo}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.trinketOne}" class="itemImg"></div>
-                    <p>${characters[1].equipment.trinketOne}</p>
+                    <p class = "descriptionText">${characters[1].equipment.trinketOne}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[1].equipment.trinketTwo}" class="itemImg"></div>
-                    <p>${characters[1].equipment.trinketTwo}</p>
+                    <p class = "descriptionText">${characters[1].equipment.trinketTwo}</p>
                     </article>`;
                     injectLocationRight.innerHTML = injectItemRight;
                     break;
                 case "charThree":
                     console.log("3");
-                    document.getElementById('charCol').style.backgroundImage = "url(../images/characters/thorn/thorn.jpg)";
+                    document.getElementById('charCol').style.backgroundImage = "url('../images/characters/thorn/thorn.jpg')";
                     injectItemLeft = '';
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.head}" class="itemImg"></div>
-                    <p>${characters[0].equipment.head}</p>
+                    <p class = "descriptionText">${characters[0].equipment.head}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.neck}" class="itemImg"></div>
-                    <p>${characters[0].equipment.neck}</p>
+                    <p class = "descriptionText">${characters[0].equipment.neck}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.shoulders}" class="itemImg"></div>
-                    <p>${characters[0].equipment.shoulders}</p>
+                    <p class = "descriptionText">${characters[0].equipment.shoulders}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.cloak}" class="itemImg"></div>
-                    <p>${characters[0].equipment.cloak}</p>
+                    <p class = "descriptionText">${characters[0].equipment.cloak}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.chest}" class="itemImg"></div>
-                    <p>${characters[0].equipment.chest}</p>
+                    <p class = "descriptionText">${characters[0].equipment.chest}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.shirt}" class="itemImg"></div>
-                    <p>${characters[0].equipment.shirt}</p>
+                    <p class = "descriptionText">${characters[0].equipment.shirt}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.tabard}" class="itemImg"></div>
-                    <p>${characters[0].equipment.tabard}</p>
+                    <p class = "descriptionText">${characters[0].equipment.tabard}</p>
                     </article>`;
                     injectItemLeft += `<article class = "item">
                     <div id = "${characters[0].equipment.wrists}" class="itemImg"></div>
-                    <p>${characters[0].equipment.wrists}</p>
+                    <p class = "descriptionText">${characters[0].equipment.wrists}</p>
                     </article>`;
                     injectLocationLeft.innerHTML = injectItemLeft;
 
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.gloves}" class="itemImg"></div>
-                    <p>${characters[0].equipment.gloves}</p>
+                    <p class = "descriptionText">${characters[0].equipment.gloves}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.belt}" class="itemImg"></div>
-                    <p>${characters[0].equipment.belt}</p>
+                    <p class = "descriptionText">${characters[0].equipment.belt}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.pants}" class="itemImg"></div>
-                    <p>${characters[0].equipment.pants}</p>
+                    <p class = "descriptionText">${characters[0].equipment.pants}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.boots}" class="itemImg"></div>
-                    <p>${characters[0].equipment.boots}</p>
+                    <p class = "descriptionText">${characters[0].equipment.boots}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.ringOne}" class="itemImg"></div>
-                    <p>${characters[0].equipment.ringOne}</p>
+                    <p class = "descriptionText">${characters[0].equipment.ringOne}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.ringTwo}" class="itemImg"></div>
-                    <p>${characters[0].equipment.ringTwo}</p>
+                    <p class = "descriptionText">${characters[0].equipment.ringTwo}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.trinketOne}" class="itemImg"></div>
-                    <p>${characters[0].equipment.trinketOne}</p>
+                    <p class = "descriptionText">${characters[0].equipment.trinketOne}</p>
                     </article>`;
                     injectItemRight += `<article class = "item">
                     <div id = "${characters[0].equipment.trinketTwo}" class="itemImg"></div>
-                    <p>${characters[0].equipment.trinketTwo}</p>
+                    <p class = "descriptionText">${characters[0].equipment.trinketTwo}</p>
                     </article>`;
                     injectLocationRight.innerHTML = injectItemRight;
                     break;
